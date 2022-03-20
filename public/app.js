26a9b1692f7b5d5698fbf5ba647e758049dc607a
Lino <aronk.code21@gmail.com>
2022-03-20 21:03:38 +0100

    Initial

---
+++ public/app.js
@@ -0,0 +1,46 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("navlinks-active");
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+const themeSlide = () => {
+  const themeBtn = document.querySelector(".themeBtn");
+  const themefy = document.querySelector(".themefy");
+
+  themefy.addEventListener("click", () => {
+    themeBtn.classList.toggle("has-dropdown");
+  });
+};
+
+const changeTheme = () => {
+  const lightButton = document.getElementById("light");
+  const darkButton = document.getElementById("dark");
+  const body = document.body;
+
+  const theme = localStorage.getItem("theme");
+
+  if (theme) {
+    body.classList.add(theme);
+  }
+
+  darkButton.onclick = () => {
+    body.classList.replace("light", "dark");
+  };
+  lightButton.onclick = () => {
+    body.classList.replace("dark", "light");
+  };
+};
+
+// Telephone Booth
+function app() {
+  navSlide();
+  themeSlide();
+  changeTheme();
+}
+app();
